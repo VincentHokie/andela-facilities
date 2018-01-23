dc32546f2e3d1dc33c2fba99e17b67c41c30a196
Vincent Asante <vincenthokie@gmail.com>
2018-01-23 18:29:24 +0300

    refactor space models

--- accommodations/space/models.py
+++ accommodations/space/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from accounts.models import User
 from django.core.validators import MinValueValidator
 
 from accommodations.models import BaseInfo
