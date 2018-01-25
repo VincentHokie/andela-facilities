091f4e0624f1bee895e96333b2d5f2285ef7d023
Vincent Asante <vincenthokie@gmail.com>
2018-01-25 18:43:45 +0300

    stop html coverage reports

--- accommodations/accommodations/settings.py
+++ accommodations/accommodations/settings.py
@@ -176,7 +176,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
     '--with-coverage',
-    '--cover-html',
+    '--cover-inclusive',
     '--cover-package=accomodations,accounts,space',
 ]
 
