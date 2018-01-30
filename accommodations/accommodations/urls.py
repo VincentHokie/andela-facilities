4195aad8ec26102c87fbdda7bd9048d04f9311ba
Vincent Asante <vincenthokie@gmail.com>
2018-01-30 15:45:22 +0300

    refactor accommodation routes to a proper spelling of accommodation

--- accommodations/accommodations/urls.py
+++ accommodations/accommodations/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include("accounts.urls")),
-    url(r'^accomodation/', include("space.urls")),
+    url(r'^accommodation/', include("space.urls")),
 ]
