7c5ed59f62830d6eea41862ebea115bd00cef188
Vincent Asante <vincenthokie@gmail.com>
2018-01-30 15:38:52 +0300

    create application reducers and associated actions

---
+++ accommodations/react-client/reducers/base.jsx
@@ -0,0 +1,42 @@
+import { SIGNIN_SUCCESS, SIGNOUT_SUCCESS } from '../types/auth.jsx';
+import { SHOW_NOTIFICATION, HIDE_NOTIFICATION } from '../types/base.jsx';
+
+const initState = {
+  token: '',
+  user: {},
+  gapi: undefined,
+  notification: false,
+  notificationType: false,
+};
+
+export default (state = initState, action) => {
+  switch (action.type) {
+    case SIGNIN_SUCCESS:
+      return {
+        ...state,
+        token: action.payload.token,
+        user: action.payload.user,
+        gapi: action.payload.gapi,
+      };
+    case SIGNOUT_SUCCESS:
+      return {
+        ...state,
+        toekn: '',
+        user: {},
+      };
+    case SHOW_NOTIFICATION:
+      return {
+        ...state,
+        notification: action.payload.notification,
+        notificationType: action.payload.notificationType,
+      };
+    case HIDE_NOTIFICATION:
+      return {
+        ...state,
+        notification: false,
+        notificationType: false,
+      };
+    default:
+      return state;
+  }
+};
