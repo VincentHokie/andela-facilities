4820305b07aa6c3156149137ec5c812419eb9dcb
Vincent Asante <vincenthokie@gmail.com>
2018-01-31 15:18:58 +0300

    remove redundant imports in home.jsx container

--- accommodations/react-client/containers/home.jsx
+++ accommodations/react-client/containers/home.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import Header from '../components/header.jsx';
-import SideBar from '../components/sidebar.jsx';
-import { signOut } from '../actions/auth.jsx'
 import Base from './base.jsx';
-import InputField from '../components/InputField.jsx';
 import DashboardWrapper from './dashboardHOC.jsx';
 
 class Home extends Base {
